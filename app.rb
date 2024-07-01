6152c19a1ece9f7de6ebe9bf2ab32a396ba63d09
Kseniia <ilijamarmelad@gmail.com>
2024-07-01 09:51:29 +0200

    data/time format

--- app.rb
+++ app.rb
@@ -1,19 +1,29 @@
+require_relative 'time_service'
+
 class App
   def call(env)
-    [status, headers, body]
+    request = Rack::Request.new(env)
+    format = request.params["format"]
+
+    if request.path_info == "/time" && format
+      time_response = TimeService.new(format)
+      time_response.format_time
+      time_response.success? ?
+           response(200, "#{time_response.time_string}\n") :
+           response(400, "Unknown time format #{time_response.errors}\n")
+    else
+      response(404, "Error!\n")
+    end
   end
 
   private
 
-  def status
-    200
+  def response(status, body)
+    [
+      status,
+      { 'Content-Type' => 'text/plain' },
+      [body]
+    ]
   end
 
-  def headers
-    { 'Content-Type' => 'text/plain' }
-  end
-
-  def body
-    ["Welcome abroad!\n"]
-  end
 end
